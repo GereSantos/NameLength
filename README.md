5628338263f54a5c49934273bba14dd71e262af2
Geremias Santos <Geremias99999@gmail.com>
2022-08-27 12:22:56 -0300

    Name Length Exercise

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Sobre o Mini- Projeto
+
+Programa aonde retorna o número de letras que um Nome contém.
+
+## Linguagens Utilizadas
+
+- Html
+- JavaScript
---
+++ script.js
@@ -0,0 +1,8 @@
+
+let lastNameLength = 0;
+const lastName = window.prompt('Insira seu nome: ');
+
+
+lastNameLength = lastName.length;
+
+alert(lastName + ',' + '\n Seu nome contém ' + lastNameLength + ' letras ');
